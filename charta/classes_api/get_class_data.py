b1125a60db8512a9380c4ee05e514becf103b04e
Tony Kim <tkim6@cs.stanford.edu>
2021-02-09 02:08:28 -0800

    Scrape classes into YAML file

--- charta/classes_api/get_class_data.py
+++ charta/classes_api/get_class_data.py
@@ -1,18 +1,46 @@
 # The code at this point has been heavily adapted from the explore-courses-api repository.
 # https://github.com/jeremyephron/explore-courses-api
 
+import sys
+import time
+import yaml
+
 from explorecourses import *
 from explorecourses import filters
+from concurrent.futures import ThreadPoolExecutor # incorporate ThreadPool later
 
+
+assert len(sys.argv) == 2, "Name of output YAML file required."
+
+# name of output YAML file
+out_file = sys.argv[1]
+
+# connect to ExploreCourses API
 connect = CourseConnection()
 
+og_time = time.time()
+
+# initialize course_dict that will became our YAML
+course_dict = {}
 # Print out all courses for 2020-2021.
 year = "2020-2021"
-num_courses = 0
+
 for school in connect.get_schools(year):
+    # loop through schools
+    course_dict[school.name] = {}
     for dept in school.departments:
+        # loop through departments within schools
+        course_dict[school.name][dept.code] = {}
+        # then courses within departments
         courses = connect.get_courses_by_department(dept.code, year=year)
         for course in courses:
-            num_courses += 1
-        print(dept, num_courses)
-print(num_courses)
+            course_dict[school.name][dept.code][course.title] = {
+                "GER": course.gers,
+                "Subject": course.subject,
+                "Code": course.code,
+                "Description": course.description
+            }
+
+f = open(out_file, "w")
+f.write(yaml.safe_dump(course_dict)) # safe_dump for the tuple requirements
+f.close()
