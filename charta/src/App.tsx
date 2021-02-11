c0f85fc73f2bb72a7672f1c33a70dd8bb76c1b79
Ruben Krueger <34781887+Ruben-Krueger@users.noreply.github.com>
2021-02-11 13:24:16 -0800

    firebase cloud firestore integration

--- charta/src/App.tsx
+++ charta/src/App.tsx
@@ -7,14 +7,36 @@ import Header from './components/header';
 import Footer from './components/footer';
 import ClassCard from './components/card';
 import Planner from './components/planner';
+// import { firebase } from '@firebase/app';
+// import 'firebase/firestore';
+import 'firebase/firestore'
+import './firebase';
+import firebase from "firebase";
+
+
+
 class App extends Component {
 
-  render() {
+    async componentDidMount() {
+        const db = firebase.firestore();
+
+        const cityRef = db.collection('users').doc('ruben1');
+        const doc = await cityRef.get();
+        if (!doc.exists) {
+            console.log('No such document!');
+        } else {
+            console.log('Document data:', doc.data());
+        }
+
+    }
+
+    render() {
     return (
       <div className="flex flex-col h-screen justify-between">
           <Header/>
           <ClassCard name="Math 51" term="Spring 2021"/>
           <ClassCard name="CS 110" term="Winter 2021"/>
+
           <Planner/>
           <Footer/>
         </div>
---
+++ charta/src/firebase.js
@@ -0,0 +1,19 @@
+import { firebase } from '@firebase/app';
+
+
+// Add the Firebase products that you want to use
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDOPu_apH2kYsTUJta6nEWwcNzSRQ2SPQk",
+    authDomain: "charta-14a8f.firebaseapp.com",
+    projectId: "charta-14a8f",
+    storageBucket: "charta-14a8f.appspot.com",
+    messagingSenderId: "284451429792",
+    appId: "1:284451429792:web:49347e70c72d2e06a098c5",
+    measurementId: "G-TTG57Q09ZF"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
