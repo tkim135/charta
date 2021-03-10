96e06d861ecb760766488762485cf586d61ced98
Tony Kim <tkim6@cs.stanford.edu>
2021-03-09 23:07:36 -0800

    Load user's planner data, convert between different quarter formats

--- charta/src/components/searchresults.tsx
+++ charta/src/components/searchresults.tsx
@@ -15,23 +15,120 @@ import Button from '@material-ui/core/Button';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import Chip from '@material-ui/core/Chip';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
+import TextField from '@material-ui/core/TextField';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import UserCourse from '../data/usercourse';
 import {Link} from 'react-router-dom';
+import { MenuItem } from '@material-ui/core';
 
 interface SearchResultState {
     course: Course,
-    loading: boolean
+    courseLoading: boolean,
+    quartersLoading: boolean,
+    userQuarters: Array<string>;
 }
 
 interface SearchResultProps {
 }
 
+interface CourseCardState {
+    addingCourse: boolean;
+    noAvailableQuarter: boolean;
+    newCode: string;
+    newQuarter: string;
+    availableQuartersToTake: Array<string>;
+    newUnits: number;
+    newGrade: string;
+    newReason: string;
+    courseAlreadyAdded: boolean;
+    invalidUnits: boolean;
+    invalidCode: boolean;
+    invalidQuarter: boolean;
+}
+
 interface CourseCardProps {
-    course: Course
+    course: Course,
+    userQuarters: Array<string>;
 }
 
 
-class CourseCard extends Component<CourseCardProps>{
+class CourseCard extends Component<CourseCardProps, CourseCardState>{
 
+    constructor(props: CourseCardProps) {
+        super(props);
+        this.state = {addingCourse: false, newCode: '', newQuarter: '',
+            noAvailableQuarter: false, availableQuartersToTake: [], newUnits: 0,
+            newGrade: '', newReason: '', courseAlreadyAdded: false,
+            invalidUnits: false, invalidCode: false, invalidQuarter: false};
+    }
+
+    async openDialog() {
+        console.log('Opening dialog!');
+        let convertedQuarters = this.props.course.terms.map((quarter: string) => {
+            // convert to user quarter format
+            let season = quarter.substring(10);
+            if (season == "Autumn") {
+                return "Fall " + quarter.substring(0,4);
+            }
+            return season + ' ' + quarter.substring(5,9);
+        });
+        let availableQuartersToTake = convertedQuarters.filter(
+            quarter => {
+                return this.props.userQuarters.includes(quarter);
+            }
+        );
+        this.setState({availableQuartersToTake: availableQuartersToTake});
+        if (availableQuartersToTake.length == 0) {
+            this.setState({noAvailableQuarter: true});
+        } else {
+            this.setState({noAvailableQuarter: false});
+        }
+        this.setState({addingCourse: true});
+    }
+
+    async closeDialog() {
+        console.log('Closing dialog!');
+        this.setState({addingCourse: false});
+    }
+
+    async addCourse() {
+        let scope = this;
+        firebase.auth().onAuthStateChanged(async function(user)  {
+            if (user) {
+                const db = firebase.firestore();
+
+                // get current courses in the given quarter
+                let quarterCourses: Array<UserCourse> = [];
+
+                // iterate over courses in this quarter
+                await db.collection(`users/${user?.uid}/${scope.state.newQuarter}`).get().then( (querySnapshot) => {
+                    console.log(scope.state.newQuarter);
+
+                     // get the course
+                     querySnapshot.forEach((doc) =>  {
+                        // let course = await this.loadCourse(doc);
+                        let courseData = doc.data();
+                        if(courseData?.ignore) return
+                        let course = new UserCourse(courseData?.code, courseData?.reason, courseData?.grade, courseData?.units, scope.state.newQuarter, courseData?.title, doc.id);
+                        quarterCourses.push(course);
+                        console.log(course);
+                    })
+                }).catch((err) => {
+                    console.log("error loading courses", err);
+                });
+                console.log(quarterCourses);
+
+                let uid = firebase.auth().currentUser?.uid;
+            } else {
+              user = null;
+            }
+        });
+    }
 
     render() {
         let course = this.props.course;
@@ -81,16 +178,42 @@ class SearchResults extends Component<SearchResultProps & RouteComponentProps, S
     constructor(props: any) {
         super(props);
         let course =  new Course("", [], "", [], "", 0, 0, [], [], "");
-        this.state = {course: course, loading: true}
+        this.state = {course: course, courseLoading: true, quartersLoading: true, userQuarters: []}
 
         this.loadCourseData = this.loadCourseData.bind(this);
     }
 
     async componentWillReceiveProps(nextProps: any) {
         await this.loadCourseData();
+        await this.loadQuarters();
         console.log('url changes');
     }
 
+    async loadQuarters() {
+        // hack around promise context
+        let scope = this;
+        firebase.auth().onAuthStateChanged(async function(user)  {
+            if (user) {
+                const db = firebase.firestore();
+                const userRef = db.collection('users').doc(user?.uid);
+                const doc = await userRef.get();
+
+                let quarters = doc.data()?.quarters;
+
+                if(quarters) {
+                    scope.setState({userQuarters: quarters});
+                }
+
+                else {
+                    scope.setState({userQuarters: []});
+                }
+
+            } else {
+              user = null;
+            }
+          });
+        this.setState({ quartersLoading : false });
+    }
 
     // this.props.match doesn't update when the url changes, but window.location.pathname does
     async loadCourseData() {
@@ -116,8 +239,7 @@ class SearchResults extends Component<SearchResultProps & RouteComponentProps, S
             );
 
             this.setState({ course : course });
-
-            this.setState({ loading : false });
+            this.setState({ courseLoading : false });
 
         })
             .catch((err) => {
@@ -128,11 +250,13 @@ class SearchResults extends Component<SearchResultProps & RouteComponentProps, S
 
     async componentDidMount() {
         await this.loadCourseData();
+        await this.loadQuarters();
     }
 
     render() {
 
         let course = this.state.course;
+        let userQuarters = this.state.userQuarters;
         return (
             <div className="flex flex-col h-screen justify-between">
 
@@ -141,7 +265,8 @@ class SearchResults extends Component<SearchResultProps & RouteComponentProps, S
                 <Container>
                     <Grid container spacing={3} justify='center'>
                         <Grid item>
-                            {this.state.loading ? <CircularProgress/> : <CourseCard course={course}/>}
+                            {(this.state.courseLoading || this.state.quartersLoading) ? <CircularProgress/> :
+                                <CourseCard course={course} userQuarters={userQuarters}/>}
 
                         </Grid>
 
