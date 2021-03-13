0c6ce99e746ff2e4b24c66883904cdcded0ce7cc
Kendra Dunsmoor <kendraj@stanford.edu>
2021-03-13 15:00:52 -0800

    added degree general reqs checker

--- charta/src/data/usercourse.tsx
+++ charta/src/data/usercourse.tsx
@@ -3,15 +3,17 @@ class UserCourse {
     reason: string;
     grade: string;
     units: number;
+    ways: string;
     term: string;
     title: string;
     id: string;
 
-    constructor(code: string, reason: string, grade: string, units: number, term: string, title: string, id: string) {
+    constructor(code: string, reason: string, grade: string, units: number, ways: string, term: string, title: string, id: string) {
         this.code = code;
         this.reason = reason;
         this.grade = grade;
         this.units = units;
+        this.ways = ways;
         this.term = term;
         this.title = title;
         this.id = id;
